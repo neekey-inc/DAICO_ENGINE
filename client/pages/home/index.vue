3e6a3b81a90d24367b2ad632b418cb97083999f6
ctoc_mitsuki <ma.090109.1997@gmail.com>
2019-06-10 18:37:50 +0900

    マージしてみる

---
+++ client/pages/home/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section>
+    <el-button><a @click="$router.push({name:'management'})">予約管理</a></el-button>
+    <el-button><a @click="$router.push({name:'setting'})">会社情報</a></el-button>
+    <el-button><a @click="$router.push({name:'analysis'})">分析</a></el-button>
+    <el-button><a @click="$router.push({name:'mail'})">メール</a></el-button>
+  </section>
+</template>
